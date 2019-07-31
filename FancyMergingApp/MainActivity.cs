aad8ac36829c3e6fd979515a32d7d5f974847ddb
Matthew Leibowitz <mattleibow@live.com>
2019-08-01 00:54:09 +0200

    ws

--- FancyMergingApp/MainActivity.cs
+++ FancyMergingApp/MainActivity.cs
@@ -12,6 +12,7 @@ namespace FancyMergingApp
 		{
 			base.OnCreate(savedInstanceState);
 			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.activity_main);
 		}
